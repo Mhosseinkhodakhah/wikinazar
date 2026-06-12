2d0fa9f7445b223ae3024c3c5d78afa9ec3a5dad
hosseinkhodakhah <hosseinkhodakhah6@gmail.com>
2026-06-12 20:37:04 +0330

    dev

--- web/src/pages/subject/[id].tsx
+++ web/src/pages/subject/[id].tsx
@@ -1,5 +1,10 @@
 import { SubjectDetail } from '../../templates/SubjectDetail';
 
-const SubjectPage = () => <SubjectDetail />;
+const SubjectPage = () => {
+console.log('it comes heeee')
+return (
+<SubjectDetail />
+)
+};
 
 export default SubjectPage;
