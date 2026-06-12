68537f3c84ded5c94c688ee13b858ee36437bfd6
hosseinkhodakhah <hosseinkhodakhah6@gmail.com>
2026-06-12 20:11:56 +0330

    dev

--- web/src/pages/subject/[id].tsx
+++ web/src/pages/subject/[id].tsx
@@ -2,15 +2,4 @@ import { SubjectDetail } from '../../templates/SubjectDetail';
 
 const SubjectPage = () => <SubjectDetail />;
 
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: 'blocking',
-  };
-}
-
-export async function getStaticProps() {
-  return { props: {} };
-}
-
 export default SubjectPage;
