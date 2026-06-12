f69e294d368a2aecf2abd1c99d25286a03e1aef3
hosseinkhodakhah <hosseinkhodakhah6@gmail.com>
2026-06-12 20:21:49 +0330

    dev

--- web/src/pages/subject/[id].tsx
+++ web/src/pages/subject/[id].tsx
@@ -1,5 +1,19 @@
+import { type GetServerSidePropsContext } from 'next';
+
 import { SubjectDetail } from '../../templates/SubjectDetail';
 
-const SubjectPage = () => <SubjectDetail />;
+interface SubjectPageProps {
+  id: string;
+}
+
+const SubjectPage = ({ id }: SubjectPageProps) => <SubjectDetail key={id} />;
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const id = ctx.params?.id as string;
+  if (!id) {
+    return { notFound: true };
+  }
+  return { props: { id } };
+}
 
 export default SubjectPage;
